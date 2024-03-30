368306d9b203abe2ed647b72e04500e9c5aa7b40
Nimal2008 <150204161+Nimal2008@users.noreply.github.com>
2024-03-30 12:54:26 -0400

    Add files via upload

--- arrow.py
+++ arrow.py
@@ -1,20 +1,36 @@
 # ArrowDraw.py
 import turtle
+
 Screen = turtle.Screen()
-t = turtle.Pen()
+# t = turtle.Pen()
 turtle.speed(0)
-turtle.turtlesize(2,2,2)
+turtle.turtlesize(2, 2, 2)
+
 
 def up():
     turtle.forward(50)
+
+
 def left():
     turtle.left(90)
+
+
 def right():
     turtle.right(90)
+
+
+def penup():
+    turtle.penup()
+
+
+def pendown():
+    turtle.pendown()
+
+
 turtle.onkeypress(up, "Up")
 turtle.onkeypress(left, "Left")
 turtle.onkeypress(right, "Right")
-turtle.onkeypress(turtle.penup(),"p")
+turtle.onkeypress(penup, "p")
+turtle.onkeypress(pendown, "d")
 turtle.listen()
 Screen.mainloop()
-
